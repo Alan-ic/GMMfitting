d4d59c72b676159cf6579e687ffbb0560d03b814
lidingX <648219931@qq.com>
2017-08-30 13:30:49 +0800

    Update and rename serialGMMfitting.h to GMMfitting.h

--- serialGMMfitting.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void serialGMMfitting(int& components, double*& weights_, double*& logweights_, double* & sumlogdiags_, double*& means, double*& chol_precisions, double* X, int* labels, int n, int d, int init_k, double tol, int maxiter);
